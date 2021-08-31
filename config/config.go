98993557caa2db4c27700c990b6d8b4df42b077d
Paulo Vital <paulo.vital@perseus.de>
2021-08-31 10:40:46 +0200

    Add new path into config structure

--- config/config.go
+++ config/config.go
@@ -6,9 +6,11 @@ package config
 import "time"
 
 type Config struct {
-	Period time.Duration `config:"period"`
+	Period 	time.Duration 	`config:"period"`
+	Path	string			`config:"path"`
 }
 
 var DefaultConfig = Config{
 	Period: 1 * time.Second,
+	Path:	".",
 }
